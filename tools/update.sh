002525db4922746f21587a7a80b53805faf3c64f
Nick Rimmer <xan@dipteam.com>
2018-01-29 04:16:03 +0700

    simplify update script

--- tools/update.sh
+++ tools/update.sh
@@ -2,9 +2,14 @@
 
 cd ~/klipper
 
-git pull
-~/klipper/scripts/install-octopi.sh
+#git pull
+git fetch --all
+git reset --hard origin/red-bot
 
-sudo service klipper stop
-make flash FLASH_DEVICE=/dev/ttyACM0
-sudo service klipper start
+#~/klipper/scripts/install-octopi.sh
+
+#sudo service klipper stop
+#make flash FLASH_DEVICE=/dev/ttyACM0
+#sudo service klipper start
+
+sudo service klipper restart
